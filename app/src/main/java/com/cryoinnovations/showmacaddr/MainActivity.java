640ea4f98bac675a46ecb7f499bc980a87d880b7
cryomeariver <51169683+cryomeariver@users.noreply.github.com>
2021-10-27 11:44:59 -0700

    added connection check
    added code to insure the device is connected to the internet

--- app/src/main/java/com/cryoinnovations/showmacaddr/MainActivity.java
+++ app/src/main/java/com/cryoinnovations/showmacaddr/MainActivity.java
@@ -2,9 +2,14 @@ package com.cryoinnovations.showmacaddr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.TargetApi;
+import android.app.Application;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkCapabilities;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.text.TextUtils;
@@ -24,6 +29,8 @@ import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "ShowMacAddress";
+
     private TextView tv_macAddress = null;
     private Pattern pat1 = Pattern.compile("\\d+\\:\\s*wlan0\\:");
     private Pattern pat2 = Pattern.compile("link/ether\\s*([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})|([0-9a-fA-F]{4}\\.[0-9a-fA-F]{4}\\.[0-9a-fA-F]{4})");
@@ -34,25 +41,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv_macAddress = findViewById(R.id.macAddress);
-        String o = shell_exec("ip addr");
-        Log.d("ADDR", o);
-        String[] lines = o.split("\n");
-        for (int lineCtr = 0; lineCtr < lines.length; lineCtr++) {
-            if (!found) {
-                Matcher matcher1 = pat1.matcher(lines[lineCtr]);
-                if (matcher1.find()) {
-                    Log.d("ADDR", "found pat1");
-                    found = true;
-                }
-            } else {
-                Matcher matcher2 = pat2.matcher(lines[lineCtr]);
-                if (matcher2.find()) {
-                    String s = matcher2.group().split(" ")[1];
-                    Log.d("ADDR", "found " + s);
-                    s = "MacAddress: " + s;
-                    tv_macAddress.setText(s);
+        if(isNetworkOnline(this)) {
+            String o = shell_exec("ip addr");
+            Log.d(TAG, o);
+            String[] lines = o.split("\n");
+            for (int lineCtr = 0; lineCtr < lines.length; lineCtr++) {
+                if (!found) {
+                    Matcher matcher1 = pat1.matcher(lines[lineCtr]);
+                    if (matcher1.find()) {
+                        Log.d("ADDR", "found pat1");
+                        found = true;
+                    }
+                } else {
+                    Matcher matcher2 = pat2.matcher(lines[lineCtr]);
+                    if (matcher2.find()) {
+                        String s = matcher2.group().split(" ")[1];
+                        Log.d(TAG, "found " + s);
+                        s = "MacAddress: " + s;
+                        tv_macAddress.setText(s);
+                    }
                 }
             }
+        } else {
+            tv_macAddress.setText(R.string.notConnected);
         }
     }
 
@@ -69,5 +80,25 @@ public class MainActivity extends AppCompatActivity {
             o = "error";
         }
         return o;
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public static boolean isNetworkOnline(Context context) {
+        boolean isOnline = false;
+        try {
+            ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            String reason = manager.getActiveNetworkInfo().getReason();
+            if(reason.equalsIgnoreCase("CONNECTED")) {
+                return true;
+            } else {
+                return false;
+            }
+            //NetworkCapabilities capabilities = manager.getNetworkCapabilities(manager.getActiveNetwork());  // need ACCESS_NETWORK_STATE permission
+            //isOnline = capabilities != null && capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return isOnline;
     }
 }
